030f989592dac7dac8756332b98c75278198b872
Ashgen <official@ashgen.com>
2019-10-08 15:05:31 +0530

    first commit

---
+++ views/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Todo App",
+  "version": "1.0.0",
+  "description": "Documentation for Todo App",
+  "title": "APIs for Todo App",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-08T06:09:48.677Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
